4137548aeb7f09fddf2d5d7a25681d2af463262c
Goraniiii <gony06011@gmail.com>
2024-03-08 00:20:10 +0900

    edit Home.jsx

---
+++ goraniiii/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+export function Home () {
+  return (
+    <>
+      <div style={{ justifyContent: "space-around", flexDirection: 'row', margin: '60px 25vw 30px' }}>
+        <h1>Goun Park</h1>
+        <div>🔨 <a href="http://github.com/goraniiii">github.com/goraniiii</a></div>
+        <div>
+          <h3>Contact</h3>
+          <div>
+            <div>📮 <a href="mailto:gony06011@gmail.com">gony06011@gmail.com</a></div>
+          </div>
+        </div>
+        <div>
+          <h3>Education</h3>
+          <div>
+            <div>
+              <div>2021 ~ current</div>
+              <div>Dongguk Univ. Computer Science and Engineering</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Skills</h3>
+          <div>
+            <li>C/C++</li>
+            <li>Java</li>
+            <li>Javascript</li>
+            <li>React</li>
+          </div>
+        </div>
+        <div>
+          <h3>Interests</h3>
+          <div>
+            <li>Web Develope</li>
+            <li>Interaction Design</li>
+            <li>Human Computer Interaction</li>
+            <li>Graphics</li>
+            <li>Media art</li>
+          </div>
+        </div>
+        <div>
+          <h3>Projects</h3>
+          <div>
+            <div>
+              <div>IJOA</div>
+              <div>웹 기반 공유형 긴급 아이돌봄 서비스</div>
+              <div>2023.03 ~ 2023.11, 2024.02 ~ current(refactoring)</div>
+            </div>
+          </div>
+        </div>
+        <div style={{ marginTop: 30 }}>Copyright 2024. Goun Park all rights reserved.</div>
+      </div>
+    </>
+  )
+}
